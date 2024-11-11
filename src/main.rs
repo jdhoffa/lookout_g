b737a0a39aa9a3a9d84754518ecf19234018b821
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-11-11 22:59:49 +0100

    make main async

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use lookout_g::Config;
 use std::env;
 use std::process;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = lookout_g::run(config) {
+    if let Err(e) = lookout_g::run(config).await {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
